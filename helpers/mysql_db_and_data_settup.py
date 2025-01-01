3579703e282a2e0598be6e97cf2888613ad0c5e0
AliAS-86 <asalman.htx@gmail.com>
2024-12-31 18:08:26 -0600

    add mysql_utils.py to handle utilities functions for mysql. add db_init for the db initializer script

--- helpers/mysql_db_and_data_settup.py
+++ helpers/mysql_db_and_data_settup.py
@@ -2,6 +2,7 @@ import mysql.connector
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from service_utils import utilities as utils
+from service_utils import mysql_utils
 import logging
 
 logging.basicConfig(
@@ -12,42 +13,21 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 
-# load mysql config from json file:
-config_file_path = utils.get_path("config_files/db_config.json")
-mysql_config = utils.json_loader(config_file_path)
-# logger.info(mysql_config)
-
 
 # Connect to server
-def connect_to_db(db_name):
-    try:
-        cnx = mysql.connector.connect(
-            host=mysql_config["config"]["host"],
-            port=mysql_config["config"]["port"],
-            user=mysql_config["config"]["user"],
-            password=mysql_config["config"]["password"]
-        )
-        logger.info(cnx)
-        cursor = cnx.cursor()
+def db_init(db_name):
+    # Connect to the mysql server
+    cnx = mysql_utils.connect_to_db()
 
-        cursor.execute(f"SHOW DATABASES LIKE '{db_name}';")
-        exists = cursor.fetchone() is not None
+   # check if database exist
+    cmd = f"SHOW DATABASES LIKE '{db_name}'"
+    result = mysql_utils.cursor_executer(cmd, cnx, return_type="fetchone")
+    logger.info(result)
+       
 
-        cursor.close()
-        cnx.close()
-
-        return exists
-    
-    except mysql.connector.Error as e:
-        logger.error(f"Error: {e}")
-        return False
-    
 if __name__ == "__main__":
-    db_name = "store_management"
-    if connect_to_db(db_name):
-        logger.info(f"Database {db_name} exists")
-    else:
-        logger.warning(f"Database {db_name} does not exist, run the db_creator function then retry to connect")
+    db_name = "test_store_management"
+    db_init(db_name)
 
         
 
---
+++ service_utils/mysql_utils.py
@@ -0,0 +1,58 @@
+import mysql.connector
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from service_utils import utilities as utils
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger(__name__)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+# load mysql config from json file:
+config_file_path = utils.get_path("config_files/db_config.json")
+mysql_config = utils.json_loader(config_file_path)
+
+def connect_to_db():
+    """A utility function to establish a connection to the mysql server and return a connection object"""
+    try:    
+        cnx = mysql.connector.connect(
+            host = mysql_config["config"]["host"],
+            port = mysql_config["config"]["port"],
+            user = mysql_config["config"]["user"],
+            password = mysql_config["config"]["password"]
+        )
+        logger.info("Database connection successful")
+        return cnx
+    
+    except mysql.connector.Error as e:
+        logger.error(f"MySQL Connection Error: {e}")
+        return
+    
+def cursor_executer(cmd, cnx=None, return_type=None):
+    """A method to execute mysql command and manage the cursor and/or connection (cnx) lifecycle"""
+    close_connection = False
+    if cnx is None:
+        cnx = connect_to_db()
+        close_connection = True
+    try:
+        with cnx.cursor() as cursor:
+            cursor.execute(cmd)
+            if return_type == "fetchall":
+                return cursor.fetchall()
+            elif return_type == "fetchone":
+                return cursor.fetchone()
+            elif return_type == "lastrowid":
+                return cursor.lastrowid
+            elif return_type == "rowcount":
+                return cursor.rowcount
+            return None
+
+    finally:
+        if close_connection:
+            cnx.close()
+
+        
